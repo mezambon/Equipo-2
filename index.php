efdc0ba1adbf7e806b958f238aab4ad0819a3074
Proyecto <saenztandil@gmail.com>
2012-04-10 11:05:28 -0300

    commit

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
+
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta charset="UTF-8&quot;">
+	<link href="css/jquery.mobile-1.0.1.css" rel="stylesheet">
+
+	<title></title>
+</head>
+
+<body class="ui-mobile-viewport ui-overlay-c">
+	<div class="ui-page ui-body-c ui-page-active" data-role="page" data-url=
+	"/test/docs/pages/page-template.html" style="min-height: 450px;" tabindex=
+	"0">
+		<div class="ui-header ui-bar-a" data-role="header">
+			<h1 class="ui-title">Twitter Sign-in</h1>
+		</div><!-- /header -->
+
+		<div class="ui-content" data-role="content">
+                    <input type="email" id="userID" name="userID" placeholder="Username/email">
+                </div><!-- /content -->
+
+		<div class="ui-footer ui-bar-a" data-role="footer">
+			<h4 class="ui-title">Final Project</h4>
+		</div><!-- /footer -->
+	</div>
+</body>
 </html>
