4d5a57c4a0aae1e54e03764bf5083015f9f61ebb
Proyecto <saenztandil@gmail.com>
2012-04-10 11:26:22 -0300

    commit

--- index.php
+++ index.php
@@ -5,15 +5,17 @@
 	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<meta charset="UTF-8&quot;">
 	<link href="css/jquery.mobile-1.0.1.css" rel="stylesheet">
-
-	<title></title>
+        <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.1.0-rc.2/jquery.mobile-1.1.0-rc.2.min.js"></script>
+	<title>Twitter API</title>
 </head>
 
 <body class="ui-mobile-viewport ui-overlay-c">
 	<div class="ui-page ui-body-c ui-page-active" data-role="page" data-url=
 	"/test/docs/pages/page-template.html" style="min-height: 450px;" tabindex=
 	"0">
-		<div class="ui-header ui-bar-a" data-role="header">
+                <br>
+                <div class="ui-header ui-bar-a" data-role="header">
 			<h1 class="ui-title">Twitter Sign-in</h1>
 		</div><!-- /header -->
 
