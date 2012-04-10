9ed8ea32d14b42fd4a5f5cb869316072a8515fb4
sebastian reynoso <gsebastianreynoso@gmail.com>
2012-04-10 16:39:47 -0300

    modelo user falta finalizar metodo login

--- Scripts/Modules/user.js
+++ Scripts/Modules/user.js
@@ -2,5 +2,22 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+define(function(){
+    function user(){
+            var username;
+            var password;
+            var connection;
+            
+            this.setUsername=function(u){this.username=u};
+            this.setPassword=function(p){this.password=p};
+            this.getUsername=function(){return username};
+            this.getPassword=function(){return password};       
+    }
+    user.prototype.login=function(){
+        //setear username y passwoerd
+        //establecer connection
+     }
+})
+        
 
 
