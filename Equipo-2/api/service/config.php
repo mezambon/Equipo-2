a2519bd3086ed0602ea4ee649bc2cbf40a60cdc5
Proyecto <saenztandil@gmail.com>
2012-04-10 09:30:46 -0300

    first commit

---
+++ Equipo-2/api/service/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Config data from twitter registered application. */
+define('CONSUMER_KEY', 'bS2nYgaty0U8hAKa0Bw');
+define('CONSUMER_SECRET', 'e60JvDR72SHD193kIXLifMyRf9lk09FoQ7Aa30WPL6I');
+define('OAUTH_CALLBACK', 'http://www.montealquila.com.ar/api/service/Callback.php');
+
+?>
