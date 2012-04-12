10ccae88f244e64d5a4031a0dda3a7a719a3907b
Juan Cruz Avellaneda <jcave777@gmail.com>
2012-04-12 18:17:40 -0300

    creado appRouter

---
+++ scripts/routers/appRouter.js
@@ -0,0 +1,22 @@
+
+define(['underscore', 'backbone'],
+    function(_, backbone){
+        var appRouter = backbone.Router.extend({
+            routes: {
+                '':'index',
+                'dashboard': 'dashboard'
+                
+            },
+            index: function(){
+                
+            },
+            dashboard: function(){
+                
+            }
+            
+        })
+    
+        
+    
+});
+
